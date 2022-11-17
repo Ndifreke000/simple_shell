74797a02e8f627e362257bce7fe5907ff17a781d
Markson Favour <95889759+Markson17@users.noreply.github.com>
2022-11-17 02:19:33 +0100

    Update _atoi.c

--- _atoi.c
+++ _atoi.c
@@ -41,12 +41,10 @@ return (0);
 *@s: the string to be converted
 *Return: 0 if no numbers in string, converted number otherwise
 */
-
 int _atoi(char *s)
 {
 int i, sign = 1, flag = 0, output;
 unsigned int result = 0;
-
 for (i = 0;  s[i] != '\0' && flag != 2; i++)
 {
 if (s[i] == '-')
